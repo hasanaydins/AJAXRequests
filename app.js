e4a5d05a9a49fb0f1f0858c9716d862e3d01991f
Hasan <33263846+hasanaydins@users.noreply.github.com>
2019-07-03 18:24:19 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app= express();
+
+app.use(express.static(__dirname + "/public")); 
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res) {
+res.render("home");
+
+});
+
+var server = app.listen(3000, function() {
+console.log("sunucu %d portunda", server.address().port);
+
+});
